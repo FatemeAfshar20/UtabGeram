f1a06d47189198ab658129abcfe06a256387204b
FatemeAfshar20 <uttab4760@gmail.com>
2020-09-04 00:52:29 -0700

    creating logic in ChatPage but There is a bug,before get in ChatPage ,sendButton is called and in page when we clicked it , it is not called

--- app/src/main/java/com/example/recyclerview/Controller/ChatPageFragment.java
+++ app/src/main/java/com/example/recyclerview/Controller/ChatPageFragment.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.recyclerview.Adapter.Holder;
 import com.example.recyclerview.Model.Users;
 import com.example.recyclerview.R;
+import com.google.android.material.button.MaterialButton;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -19,6 +21,9 @@ public class ChatPageFragment extends Fragment {
 
     private CircleImageView mProfileImage;
     private TextView mUserName;
+    private EditText mMsg;
+    private MaterialButton mBtnSend;
+    private ViewGroup mLayChatPlace;
     public ChatPageFragment() {
         // Required empty public constructor
     }
@@ -35,17 +40,33 @@ public class ChatPageFragment extends Fragment {
         View view= inflater.inflate(R.layout.chat_page, container, false);
         Users user= (Users) getActivity().getIntent().getSerializableExtra(Holder.EXTRA_USER_INFO);
         findElem(view);
-        bind(user);
+        bindInfo(user);
+        setListener();
         return view;
     }
 
     private void findElem(View view){
         mProfileImage =view.findViewById(R.id.profile);
         mUserName=view.findViewById(R.id.username);
+        mBtnSend=view.findViewById(R.id.btn_send);
+        mMsg=view.findViewById(R.id.msg);
+        mLayChatPlace=view.findViewById(R.id.chat_area);
     }
 
-    public void bind(Users user){
+    public void bindInfo(Users user){
         mProfileImage.setImageResource(user.getImageId());
         mUserName.setText(user.getUserName());
+    }
+
+    private void setListener(){
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg=mMsg.getText().toString();
+                TextView msgSent=new TextView(getContext());
+                msgSent.setText(msg);
+                mLayChatPlace.addView(msgSent);
+            }
+        });
     }
 }
